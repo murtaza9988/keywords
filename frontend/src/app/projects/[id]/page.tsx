ad144af1e29a1c181226413ebf946808c65d3241
Tiffany <34196755+Vibe-coding-nvm-delete-repo@users.noreply.github.com>
2026-01-14 01:39:14 -0500

    fix: disable ssr for project detail page

--- frontend/src/app/projects/[id]/page.tsx
+++ frontend/src/app/projects/[id]/page.tsx
@@ -1,14 +1,18 @@
 "use client";
+import dynamic from 'next/dynamic';
 import Layout from './components/Layout';
-import ProjectDetail from './components/ProjectDetail';
 import { Provider } from 'react-redux';
-import { store } from '@/store/store'
+import { store } from '@/store/store';
+
+const ProjectDetail = dynamic(() => import('./components/ProjectDetail'), {
+  ssr: false,
+});
 export default function ProjectDetailPage() {
   return (
     <Provider store={store}>
-    <Layout>
-      <ProjectDetail />
-    </Layout>
+      <Layout>
+        <ProjectDetail />
+      </Layout>
     </Provider>
   );
 }
